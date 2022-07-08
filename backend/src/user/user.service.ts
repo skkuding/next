7f936f11480f986906a6738193c70f8543e770dd
realsehwan@gmail.com <realsehwan@gmail.com>
2022-07-08 09:23:19 +0000

    ﻿refactor: add exceptions and change method name
    원래 throw new 'string'으로 되어있던 것을 exception class를 사용하도록 변경합니다.
    nodemailer를 사용하는 부분에는 custom exception class를 만들어 사용했습니다.
    sendPwResetEmail을 sendPwResetToken으로 변경합니다.

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,10 +1,18 @@
-import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common'
+import {
+  CACHE_MANAGER,
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException
+} from '@nestjs/common'
 import { Cache } from 'cache-manager'
 import { randomBytes } from 'crypto'
 import { PrismaService } from 'src/prisma/prisma.service'
-import nodemailer from 'nodemailer'
+import * as nodemailer from 'nodemailer'
 import { ConfigService } from '@nestjs/config'
 import { encrypt } from 'src/auth/common/hash'
+import { InvalidSMTPException } from './invalidSMTP.exception'
+import SMTPTransport from 'nodemailer/lib/smtp-transport'
 
 @Injectable()
 export class UserService {
@@ -29,23 +37,67 @@ export class UserService {
     })
   }
 
-  async sendEmail(email: string, html: string): Promise<void> {
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: this.config.get('NODEMAILER_USER'),
-        pass: this.config.get('NODEMAILER_PASS')
-      }
-    })
-    await transporter.sendMail({
-      from: this.config.get('NODEMAILER_USER'),
-      to: email,
-      subject: 'Using Nodemailer',
-      html: html
-    })
+  async sendEmail(
+    email: string,
+    html: string
+  ): Promise<SMTPTransport.SentMessageInfo> {
+    try {
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: this.config.get('NODEMAILER_USER'),
+          pass: this.config.get('NODEMAILER_PASS')
+        }
+      })
+
+      const sentMessageInfo = await transporter.sendMail({
+        to: email,
+        subject: 'Using Nodemailer',
+        html: html
+      })
+
+      return sentMessageInfo
+    } catch (error) {
+      throw new InvalidSMTPException(error)
+    }
   }
 
-  async sendPwResetEmail(email: string): Promise<string> {
+  private async getTokenFromCache(key: string): Promise<string> {
+    try {
+      const storedToken: string = await this.cacheManager.get(key)
+      return storedToken
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Cache problem; Cannot get the token'
+      )
+    }
+  }
+
+  private async setTokenInCache(
+    key: string,
+    token: string,
+    timeToLive: number
+  ): Promise<void> {
+    try {
+      await this.cacheManager.set(key, token, { ttl: timeToLive })
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Cache Problem; Cannot save the token'
+      )
+    }
+  }
+
+  private async deleteTokenFromCache(key: string): Promise<void> {
+    try {
+      await this.cacheManager.del(key)
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Cache Problem; Cannot delete the token'
+      )
+    }
+  }
+
+  async sendPwResetToken(email: string): Promise<string> {
     const user = await this.prisma.user.findUnique({
       where: {
         email: email
@@ -53,7 +105,9 @@ export class UserService {
     })
 
     if (user === null) {
-      throw 'user not found, please sign up first'
+      throw new NotFoundException(
+        `Cannot find a registered user whose email address is ${email}`
+      )
     }
 
     const token: string = randomBytes(24).toString('base64url')
@@ -64,7 +118,7 @@ export class UserService {
     <div>http://localhost:5000/user/${user.id}/password/reset/${token}</div>`
     )
 
-    await this.cacheManager.set(`pw-reset:${user.id}`, token, { ttl: 300 })
+    await this.setTokenInCache(`pw-reset:${user.id}`, token, 300)
 
     return 'Password reset link was sent to your email'
   }
@@ -74,28 +128,34 @@ export class UserService {
     resetToken: string,
     newPassword: string
   ): Promise<string> {
-    const storedResetToken: string = await this.cacheManager.get(
+    const storedResetToken: string = await this.getTokenFromCache(
       `pw-reset:${userId}`
     )
 
     if (storedResetToken === null) {
-      throw 'token not found'
+      throw new NotFoundException('Token Not Found')
     }
 
     if (resetToken !== storedResetToken) {
-      throw 'invalid token'
+      throw new NotFoundException('Invalid Token')
     }
 
-    await this.prisma.user.update({
-      where: {
-        id: userId
-      },
-      data: {
-        password: await encrypt(newPassword)
-      }
-    })
+    try {
+      await this.prisma.user.update({
+        where: {
+          id: userId
+        },
+        data: {
+          password: await encrypt(newPassword)
+        }
+      })
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'DB error caused while updating new password'
+      )
+    }
 
-    await this.cacheManager.del(`pw-reset:${userId}`)
+    await this.deleteTokenFromCache(`pw-reset:${userId}`)
 
     return 'Password Reset successfully'
   }
