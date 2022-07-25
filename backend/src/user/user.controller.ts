a96d7c0a4e5dc1735874e45ce2c192800f2c3870
minseo999 <alstj778@gmail.com>
2022-07-25 17:50:39 +0000

    feat(be): implement signup feature

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  InternalServerErrorException,
+  Post,
+  UnprocessableEntityException
+} from '@nestjs/common'
+import { UnprocessableDataException } from 'src/common/exception/business.exception'
+import { SignUpDto } from './dto/sign-up.dto'
 import { UserService } from './user.service'
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('/signup')
+  async signUp(@Body() signUpDto: SignUpDto) {
+    try {
+      await this.userService.signUp(signUpDto)
+      return
+    } catch (err) {
+      if (err instanceof UnprocessableDataException) {
+        throw new UnprocessableEntityException(err.message)
+      }
+      throw new InternalServerErrorException()
+    }
+  }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { SignUpDto } from './dto/sign-up.dto'
+import { UnprocessableDataException } from 'src/common/exception/business.exception'
+import { User, UserProfile } from '@prisma/client'
+import { encrypt } from 'src/common/hash'
+import { UserProfileData } from './interface/user-profile-data.interface'
+import { GroupService } from 'src/group/group.service'
+import { UserGroupData } from 'src/group/interface/user-group-data.interface'
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly groupService: GroupService
+  ) {}
 
   async getUserRole(userId: number) {
     return this.prisma.user.findUnique({
@@ -18,5 +28,67 @@ export class UserService {
     return this.prisma.user.findUnique({
       where: { username }
     })
+  }
+
+  async signUp(signUpDto: SignUpDto): Promise<User> {
+    // TODO: 인증된 Email인지 확인 Cache or JWT
+
+    const duplicatedUser = await this.prisma.user.findUnique({
+      where: {
+        username: signUpDto.username
+      }
+    })
+
+    if (duplicatedUser) {
+      throw new UnprocessableDataException('Username already exists')
+    }
+
+    const user = await this.createUser(signUpDto)
+
+    const userProfileData: UserProfileData = {
+      user_id: user.id,
+      real_name: signUpDto.real_name
+    }
+    await this.createUserProfile(userProfileData)
+
+    await this.registerUserToPublicGroup(user.id)
+
+    return user
+  }
+
+  async createUser(signUpDto: SignUpDto): Promise<User> {
+    const encryptedPassword = await encrypt(signUpDto.password)
+
+    return await this.prisma.user.create({
+      data: {
+        username: signUpDto.username,
+        password: encryptedPassword,
+        email: signUpDto.email,
+        last_login: null
+      }
+    })
+  }
+
+  async createUserProfile(
+    userProfileData: UserProfileData
+  ): Promise<UserProfile> {
+    return await this.prisma.userProfile.create({
+      data: {
+        real_name: userProfileData.real_name,
+        user: {
+          connect: { id: userProfileData.user_id }
+        }
+      }
+    })
+  }
+
+  async registerUserToPublicGroup(user_id: number) {
+    const userGroupData: UserGroupData = {
+      user_id,
+      group_id: 1,
+      is_registerd: true,
+      is_group_manager: false
+    }
+    await this.groupService.createUserGroup(userGroupData)
   }
 }
