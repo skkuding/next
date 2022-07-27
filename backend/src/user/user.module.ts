bc8f90d07aa82fbfa8d5c6d9b0652c5836db660d
minseo999 <alstj778@gmail.com>
2022-07-27 08:42:44 +0000

    fix(be): fix circular dependency between auth and user

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common'
+import { forwardRef, Module } from '@nestjs/common'
+import { AuthModule } from 'src/auth/auth.module'
+import { GroupModule } from 'src/group/group.module'
 import { UserController } from './user.controller'
 import { UserService } from './user.service'
 
 @Module({
+  imports: [forwardRef(() => AuthModule), GroupModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService]
